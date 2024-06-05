9610eb5adec6fa83f3dda18993fd2eba23f1a511
omarchena <oscar.marchena@hotmail.com>
2024-06-05 03:44:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = [
+        {
+            "skill": "HTML",
+            "level": "90%"
+        },
+        {
+            "skill": "CSS",
+            "level": "80%"
+        },
+        {
+            "skill": "JavaScript",
+            "level": "70%"
+        },
+        {
+            "skill": "PHP",
+            "level": "60%"
+        }
+    ];
+    return skills;
+}
